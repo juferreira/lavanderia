1e64222e39378b426b1576f9ccd7c73f5ca9b825
Vagner <vagner32@gmail.com>
2018-08-03 15:56:30 -0300

    ajustes nos models

--- lrb/admin.py
+++ lrb/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+#from .models import Reserva
+from .models import Apartamento
+from .models import Maquina
+from .models import Morador
 
 # Register your models here.
+
+#admin.site.register(Reserva)
+
+admin.site.register(Apartamento)
+admin.site.register(Morador)
+admin.site.register(Maquina)
--- teste.py
+++ teste.py
@@ -2,31 +2,13 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+
 class Apartamento(models.Model):
     unidade = models.IntegerField(primary_key=True)
-    morador = models.ForeignKey(Morador)
-
-    def __str__(self):
-           return str(self.unidade)
-
-class Morador(models.Model):
-    apartamento = models.ForeignKey(Apartamento)
-    nome = models.CharField(max_length=200)
-    auto_increment_id = models.AutoField(primary_key=True)
-
-    class Meta:
-        verbose_name_plural = "Moradores"
-
-    def __str__(self):
-        return self.nome
-  
-
+ 
 class Maquina(models.Model):
     auto_increment_id = models.AutoField(primary_key=True)
     numero = models.IntegerField()
-    def __str__(self):
-        return str(self.numero)
-
 
 class Reserva(models.Model):
     data_uso_maquina = models.DateTimeField(blank=True, null=False, primary_key=True)
@@ -34,5 +16,13 @@ class Reserva(models.Model):
     maquina = models.ForeignKey(Maquina, on_delete=models.CASCADE)
     data_criacao_reserva = models.DateTimeField(
             default=timezone.now)
-    class meta:
-        unique_together = (('data_uso_maquina','apartamento_responsavel','maquina'),) 
+
+    def __str__(self):
+        return 'Unidade: ' + str(self.apartamento_responsavel.unidade) + ' - Data da utilização: ' + self.data_uso_maquina.strftime("%Y-%m-%d %H:%M:%S") 
+
+
+
+    
+    
+        
+
